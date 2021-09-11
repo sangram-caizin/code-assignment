5372a4e05d0fae5668c543c644f48864184bb8f2
Sangram <1736005+sangram-chavan@users.noreply.github.com>
2021-09-11 22:36:03 +0530

    First Commit

---
+++ src/app/employee-list/employee-list.module.ts
@@ -0,0 +1,32 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCommonModule } from '@angular/material/core';
+import { MatChipsModule, MatChip } from '@angular/material/chips';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { EmployeeListComponent } from './employee-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCommonModule,
+    MatChipsModule
+  ],
+  declarations: [EmployeeListComponent],
+  exports: [
+    EmployeeListComponent,
+    MatInputModule,
+    MatCommonModule,
+    MatChipsModule
+  ]
+})
+export class EmployeeListModule {}
